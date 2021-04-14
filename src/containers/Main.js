b31fff40bba813f6a3fd675766ee75ffea79d0d1
willwalsh96 <williamtecumsehwalsh@gmail.com>
2021-04-14 11:10:46 -0500

    turned main.js and podcast.js back on just to get this to build

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -11,7 +11,7 @@ import Blogs from "./blogs/Blogs";
 import Contact from "./contact/Contact";
 import Footer from "../components/footer/Footer";
 import Talks from "./talks/Talks";
-//import Podcast from "./podcast/Podcast";
+import Podcast from "./podcast/Podcast";
 import Top from "./topbutton/Top";
 import Twitter from "./twitter-embed/twitter";
 import Profile from "./profile/Profile";
