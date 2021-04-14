35812f3dc089b5375db5e0429482a3341541220e
willwalsh96 <williamtecumsehwalsh@gmail.com>
2021-04-14 11:42:48 -0500

    forgot a lin ein podcast.j

--- src/containers/podcast/Podcast.js
+++ src/containers/podcast/Podcast.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Podcast.css";
-//import { podcastSection } from "../../portfolio";
+import { podcastSection } from "../../portfolio";
 import { Fade } from "react-reveal";
 
 export default function Podcast() {
